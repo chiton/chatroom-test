03bf92bca797a87b00dbd8a326825ce861f9a386
Adrian Anton <adrian.anton@southworks.net>
2015-07-20 14:43:53 -0300

    Update users resource.

--- Chatroom/routes/users.js
+++ Chatroom/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res) {
-    res.send('respond with a resource');
+    res.send('This is a user');
 });
 
 module.exports = router;
